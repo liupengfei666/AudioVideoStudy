faa146635b7861b15edfa08c5d207138e647f18a
lweis <lweis.liu@powervision.me>
2019-01-25 15:36:28 +0800

    录制PCM数据并播放

--- app/src/main/java/com/lewis/audiovideostudy/activity/AudioRecordTrackActivity.java
+++ app/src/main/java/com/lewis/audiovideostudy/activity/AudioRecordTrackActivity.java
@@ -31,6 +31,9 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
+/**
+ * 录制PCM数据并播放，实现PCM数据转WAV
+ */
 public class AudioRecordTrackActivity extends AppCompatActivity {
 
     /**
@@ -56,6 +59,9 @@ public class AudioRecordTrackActivity extends AppCompatActivity {
 
     private boolean isRecording;//是否正在录制
     private boolean isPlaying;//是否正在播放
+
+    private AudioRecordThread mAudioRecordThread; //录制线程
+    private AudioTrackPlayThread mAudioTrackPlayThread;//播放线程
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -84,17 +90,32 @@ public class AudioRecordTrackActivity extends AppCompatActivity {
             Toast.makeText(this, "正在录制", Toast.LENGTH_SHORT).show();
             return;
         }
-        RecordThread recordThread = new RecordThread();
-        recordThread.execute();
+        //1
+//        RecordThread recordThread = new RecordThread();
+//        recordThread.execute();
+        //2
+        if (null != mAudioRecordThread) {
+            //若线程不为空,则中断线程
+            mAudioRecordThread.interrupt();
+            mAudioRecordThread = null;
+        }
+        mAudioRecordThread = new AudioRecordThread();
+        mAudioRecordThread.start();
         mRecordStatus.setText(getString(R.string.recording));
     }
 
     public void stopRecord(View view) {
-        if (isRecording) {
-            isRecording = false;
-            mRecordStatus.setText(getString(R.string.stop_recording));
+        //1
+//        if (isRecording) {
+//            isRecording = false;
+//            mRecordStatus.setText(getString(R.string.stop_recording));
+//        }
+        //2
+        if (null != mAudioRecordThread) {
+            mAudioRecordThread.interrupt();
+            mAudioRecordThread = null;
         }
-
+        isRecording = false;
     }
 
     public void startPlay(View view) {
@@ -106,16 +127,27 @@ public class AudioRecordTrackActivity extends AppCompatActivity {
             Toast.makeText(this, "正在录制", Toast.LENGTH_SHORT).show();
             return;
         }
-        PlayThread playThread = new PlayThread();
-        playThread.execute();
+        //1
+//        PlayThread playThread = new PlayThread();
+//        playThread.execute();
+        //2
+        File file = new File(Environment.getExternalStorageDirectory().getAbsolutePath(), "audio.pcm");
+        mAudioTrackPlayThread = new AudioTrackPlayThread(file);
+        mAudioTrackPlayThread.start();
         mPlayStatus.setText(getString(R.string.playing));
     }
 
     public void stopPlay(View view) {
-        if (isPlaying) {
-            isPlaying = false;
-            mPlayStatus.setText(getString(R.string.stop_play));
+        //1
+//        if (isPlaying) {
+//            isPlaying = false;
+//            mPlayStatus.setText(getString(R.string.stop_play));
+//        }
+        if (null != mAudioTrackPlayThread) {
+            mAudioTrackPlayThread.interrupt();
+            mAudioTrackPlayThread = null;
         }
+        isPlaying = false;
     }
 
     /**
@@ -231,6 +263,144 @@ public class AudioRecordTrackActivity extends AppCompatActivity {
                 e.printStackTrace();
             }
             return null;
+        }
+    }
+
+    /**
+     * 录制线程
+     */
+    private class AudioRecordThread extends Thread {
+        AudioRecord mAudioRecord;
+        int minBufferSize = 10240;
+
+        AudioRecordThread() {
+            /*
+             * 获取音频缓冲最小的大小
+             */
+            minBufferSize = AudioRecord.getMinBufferSize(SAMPLE_RATE, CHANNEL_CONFIG, AUDIO_FORMAT);
+            /*
+             * 参数1：音频源
+             * 参数2：采样率 主流是44100
+             * 参数3：声道设置 MONO单声道 STEREO立体声
+             * 参数4：编码格式和采样大小 编码格式为PCM,主流大小为16BIT
+             * 参数5：采集数据需要的缓冲区大小
+             */
+            mAudioRecord = new AudioRecord(MediaRecorder.AudioSource.MIC,
+                    SAMPLE_RATE, CHANNEL_CONFIG, AUDIO_FORMAT, minBufferSize);
+        }
+
+        @Override
+        public void run() {
+            //将状态置为录制
+
+            Log.d("audio", "录制开始");
+            try {
+                byte[] bytes = new byte[minBufferSize];
+
+                File file = new File(Environment.getExternalStorageDirectory().getAbsolutePath(), "audio.pcm");
+
+                FileOutputStream fos = new FileOutputStream(file);
+
+                //开始录制
+                mAudioRecord.startRecording();
+                isRecording = true;
+
+                while (isRecording && !isInterrupted()) {
+                    int read = mAudioRecord.read(bytes, 0, bytes.length);
+                    //若读取数据没有出现错误，将数据写入文件
+                    if (AudioRecord.ERROR_INVALID_OPERATION != read) {
+                        fos.write(bytes, 0, read);
+                        fos.flush();
+                    }
+                }
+                mAudioRecord.stop();//停止录制
+                fos.close();//关流
+
+            } catch (Exception e) {
+                e.printStackTrace();
+
+            }
+            isRecording = false;
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    mRecordStatus.setText(getString(R.string.stop_recording));
+                }
+            });
+            Log.d("audio", "录制结束");
+        }
+
+    }
+
+    /**
+     * 播放音频线程
+     */
+    private class AudioTrackPlayThread extends Thread {
+        AudioTrack mAudioTrack;
+        int minBufferSize = 10240;
+        File autoFile; //要播放的文件
+
+        //        @RequiresApi(api = Build.VERSION_CODES.M)
+        AudioTrackPlayThread(File file) {
+            setPriority(MAX_PRIORITY);
+            autoFile = file;
+            //播放缓冲的最小大小
+            minBufferSize = AudioTrack.getMinBufferSize(SAMPLE_RATE, AudioFormat.CHANNEL_OUT_MONO, AUDIO_FORMAT);
+            // 创建用于播放的 AudioTrack
+            mAudioTrack = new AudioTrack(
+                    new AudioAttributes.Builder()
+                            //我发现这个参数设置会影响播放，如果不行就多试几个，我在这儿花了好多时间
+                            .setUsage(AudioAttributes.USAGE_ALARM)
+                            .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+                            .build(),
+                    new AudioFormat.Builder()
+                            .setEncoding(AUDIO_FORMAT)
+                            .setSampleRate(SAMPLE_RATE)
+                            .setChannelMask(AudioFormat.CHANNEL_OUT_MONO)
+                            .build(), minBufferSize, AudioTrack.MODE_STREAM, AudioManager.AUDIO_SESSION_ID_GENERATE);
+
+        }
+
+        @Override
+        public void run() {
+
+            Log.d("audio", "播放开始");
+            try {
+                FileInputStream fis = new FileInputStream(autoFile);
+                mAudioTrack.play();
+                isPlaying = true;
+                byte[] bytes = new byte[minBufferSize];
+
+                while (isPlaying) {
+                    int read = fis.read(bytes);
+                    //若读取有错则跳过
+                    if (AudioTrack.ERROR_INVALID_OPERATION == read
+                            || AudioTrack.ERROR_BAD_VALUE == read) {
+                        continue;
+                    }
+
+                    if (read != 0 && read != -1) {
+                        mAudioTrack.write(bytes, 0, minBufferSize);
+                    }
+                    if (read == -1) {
+                        isPlaying = false;
+                    }
+                }
+                mAudioTrack.stop();
+                mAudioTrack.release();//释放资源
+                fis.close();//关流
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    mPlayStatus.setText(getString(R.string.stop_play));
+                }
+            });
+            Log.d("audio", "播放停止");
         }
     }
 }
